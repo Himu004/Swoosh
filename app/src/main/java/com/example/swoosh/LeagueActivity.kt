66b1a6f7efa87bcd86fd856e2743b48206458e36
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-01 22:35:14 +0600

    skill activity layout added!

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -7,12 +7,28 @@ import android.view.View
 import androidx.appcompat.app.AppCompatViewInflater
 
 class LeagueActivity : BaseActivity() {
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun onMensClicked(view: View) {
+
+    }
+
+    fun onWomenBtnCliked(view: View) {
+
+    }
+
+    fun onCoedClicked(view: View) {
+
+    }
+
     fun leagueNextClicked(view: View) {
         val SkillActivity = Intent (this, SkillActivity::class.java)
         startActivity(SkillActivity)
     }
+
+
 }
