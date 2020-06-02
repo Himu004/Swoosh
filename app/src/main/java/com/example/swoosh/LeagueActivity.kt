95733dc76654fb175fc64691b8886b1748f4104b
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-02 13:21:54 +0600

    skill activity layout added!

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatViewInflater
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
     var selectedLeague = ""
@@ -14,20 +16,42 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun onMensClicked(view: View) {
+        womenLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
 
+        selectedLeague = "mens"
     }
 
     fun onWomenBtnCliked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
 
+        selectedLeague = "womens"
     }
 
     fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womenLeagueBtn.isChecked = false
 
+        selectedLeague = "coed"
     }
 
+
     fun leagueNextClicked(view: View) {
-        val SkillActivity = Intent (this, SkillActivity::class.java)
-        startActivity(SkillActivity)
+        if (selectedLeague != "") {
+            val SkillActivity = Intent (this, SkillActivity::class.java)
+            SkillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(SkillActivity)
+            mensLeagueBtn.isChecked = false
+            womenLeagueBtn.isChecked = false
+            coedLeagueBtn.isChecked = false
+
+            selectedLeague = ""
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 
 
