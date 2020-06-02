67493d57bf87f3e9407c1f052f3483bc9745c12c
Himu Chowdhury <himu.chowdhury01@gmail.com>
2020-06-02 19:22:44 +0600

    finish activity logic implemented

--- app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Toast
 import com.example.swoosh.Utilities.EXTRA_LEAGUE
 import com.example.swoosh.R
+import com.example.swoosh.Utilities.EXTRA_SKILL
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
@@ -17,12 +18,12 @@ class SkillActivity : BaseActivity() {
        league = intent.getStringExtra(EXTRA_LEAGUE)
     }
 
-    fun onBeginnerClick(view: View) {
+    fun onBallerSkillClick(view: View){
         beginnerSkillBtn.isChecked = false
         skill = "baller"
     }
 
-    fun onBallerSkillClick(view: View){
+    fun onBeginnerClick(view: View) {
         ballerSkillBtn.isChecked = false
         skill = "beginner"
     }
@@ -32,7 +33,12 @@ class SkillActivity : BaseActivity() {
 
         if (skill != "") {
             val finishActivity = Intent(this, FinishActivity::class.java)
+            finishActivity.putExtra(EXTRA_LEAGUE, league)
+            finishActivity.putExtra(EXTRA_SKILL, skill)
             startActivity(finishActivity)
+            beginnerSkillBtn.isChecked = false
+            ballerSkillBtn.isChecked = false
+
         }else {
             Toast.makeText(this, "Please select a skill level", Toast.LENGTH_LONG).show()
         }
